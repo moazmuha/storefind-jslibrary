ba3eb044bec4c686c0dc6c35c33b7652f926de57
Muhammad Moaz <m.moaz@mail.utoronto,ca>
2020-07-26 19:15:51 -0400

    heroku

--- server.js
+++ server.js
@@ -11,5 +11,5 @@ app.get("/", function (req, res) {
     res.send("<h1>Hello World!</h1>")
   })
 
-app.listen(process.env.PORT || 3000, 
+app.listen(process.env.PORT || 9000, 
 	() => console.log("Server is running..."));
